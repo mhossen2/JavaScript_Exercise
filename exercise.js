07cec9b763b45519d99ec7d47456295ebe721520
mhossen2 <81900476+mhossen2@users.noreply.github.com>
2026-05-13 13:01:51 +0100

    Add files via upload

---
+++ exercise.js
@@ -0,0 +1,51 @@
+//Switch statement
+const grade = 'P'
+
+switch (grade) {
+  case 'A':
+    console.log('you got a grade A')
+    break
+  case 'B':
+    console.log('you got a grade B')
+    break
+  case 'C':
+    console.log('you got a grade C')
+    break
+  case 'D':
+    console.log('you got a grade D')
+    break
+  case 'E':
+    console.log('you got a grade E')
+    break
+
+  default:
+    console.log('not a valid grade')
+}
+
+//Function declaration //
+greet()
+function greet() {
+  console.log('I am Md Hossen')
+}
+
+//Function Expression
+
+const calcArea = function (radius) {
+  let area = 3.14 * radius ** 2
+  return area
+}
+const a = calcArea(5)
+console.log(a)
+
+//Difference between function and method
+//Function
+const welcome = () => {
+  return 'Hello friend'
+}
+let output = welcome()
+console.log(output)
+
+// Method
+const position = 'engineer'
+let result = position.toUpperCase()
+console.log(result)
